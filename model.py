ff510dd391550183f99d6d68387fe7435b0d280c
Giraffe <giraffe@tree.com>
2020-06-05 19:41:12 +0700

    change model.py

--- model.py
+++ model.py
@@ -11,9 +11,9 @@ def create_model():
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10))
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(10))
     return model
 
 
